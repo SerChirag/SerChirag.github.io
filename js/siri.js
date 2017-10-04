081dfb2ab0080a710e057a2776a881017d706cd5
Chirag Vashist <chiragvashist007@gmail.com>
2017-10-04 06:30:02 +0530

    Commitus Finalus

---
+++ js/siri.js
@@ -0,0 +1,10 @@
+var SW = new SiriWave({
+	width: $(window).width(),
+	height: $(window).width()/3.7,
+	speed: 0.012,
+	amplitude: 1,
+	container: document.getElementById('siric'),
+	autostart: true,
+	color: "#7B1FA2",
+	frequency: 3,
+});
